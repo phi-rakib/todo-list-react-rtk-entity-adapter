905f6e75d73ec897b724c6dffcca6991a768e205
rakib <phi.rakib@gmail.com>
2021-05-27 16:13:16 +0600

    added TodoPage component in app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TodoPage from "./features/todo/TodoPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Todo List</h1>
+      <TodoPage />
     </div>
   );
 }

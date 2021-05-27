41e170c79275210915fe74cd5539884b2da8186d
rakib <phi.rakib@gmail.com>
2021-05-27 16:05:57 +0600

    fixed import error

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { todosReducer } from "../features/todo/todoSlice";
+import todosReducer from "../features/todo/todoSlice";
 
 export const store = configureStore({
   reducer: {

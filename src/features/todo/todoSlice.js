6129a177570cb5032b17909887b8461265cc0c63
rakib <phi.rakib@gmail.com>
2021-05-27 15:32:19 +0600

    created initial state, added loading as well as error in their default state value

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,7 +1,10 @@
 import { createEntityAdapter } from "@reduxjs/toolkit";
 
 const todoAdapter = createEntityAdapter({
-  sortComparer: (a, b) => b.id - a.id
+  sortComparer: (a, b) => b.id - a.id,
 });
 
-
+export const initialState = todoAdapter.getInitialState({
+  loading: false,
+  error: null,
+});

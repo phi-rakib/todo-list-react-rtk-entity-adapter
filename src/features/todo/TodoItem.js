1814a9a27b44d9bcb56d2fba06b21a2552f999b0
rakib <phi.rakib@gmail.com>
2021-05-31 23:27:49 +0600

    included DeleteTodo component to delete todo items

--- src/features/todo/TodoItem.js
+++ src/features/todo/TodoItem.js
@@ -1,10 +1,13 @@
 import React from "react";
+import DeleteTodo from "./DeleteTodo";
 import "./TodoItem.css";
 
 const TodoItem = ({ todo }) => {
+  const { id } = todo;
   return (
     <li className={todo.completed ? "job_done" : ""}>
       {todo.title}
+      <DeleteTodo id={id} />
     </li>
   );
 };
